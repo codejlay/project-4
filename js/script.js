90f61a10483023172e13147e0b6f900f798da450
codejlay <jlay@outlook.com>
2015-10-22 13:16:42 -0700

    updated files

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+(function(){
 
 angular.module('myApp', []) 
   // .run(['$rootScope', function($rootScope){
@@ -6,30 +6,51 @@ angular.module('myApp', [])
   // }])
   .controller('RegisterFormCtrl', ['$scope', function($scope){
 
-  // $scope.showValidation = false;
+  $scope.showValidation = false;
 
-      $scope.submitRegistration = function(e, form) {
-        e.preventDefault();
-        console.log(form);
-        
+    $scope.enter = function(e) {
+      e.preventDefault(); 
+
+     if ($scope.myForm.$invalid) {
+      $scope.showValidation = true;
+      alert('hola');
+
       }
+
+      // $scope.submitRegistration = function(e, form) {
+      //   e.preventDefault();
+      //   console.log(form);
+        
+    }// }
+
   }]) 
 
-    .controller('ReportFormCtrl', ['$scope', function($scope) {
+    // .controller('ReportFormCtrl', ['$scope', function($scope) {
 
-      $scope.submitReport = function(e, form) {
-        e.preventDefault();
-        console.log(form);
+    //   $scope.submitReport = function(e, form) {
+    //     e.preventDefault();
+    //     console.log(form);
 
-      }
-}]);
+    //   }
+
+     //  $scope.enter = function(e, myForm) {
+     //  e.preventDefault(); 
+
+     // if ($scope.myForm.$invalid) {
+     //  $scope.showValidation = true;
+
+     //  }
+
+
+// }]);
 
    //  $scope.enter = function(e) {
    //    e.preventDefault(); 
 
-    //  if ($scope.myForm.$invalid) {
+   //   if ($scope.myForm.$invalid) {
    //    $scope.showValidation = true;
 
    //    }
 
    // } 
+ })();
